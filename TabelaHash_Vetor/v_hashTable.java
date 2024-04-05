64f2b46445c3e986a60def9c658487e943cecdda
VenixBR <148165925+VenixBR@users.noreply.github.com>
2024-04-05 18:20:52 -0300

    Add files via upload

--- TabelaHash_Vetor/v_hashTable.java
+++ TabelaHash_Vetor/v_hashTable.java
@@ -1,38 +1,55 @@
 public class v_hashTable {
 
     private Veiculo[] vetor;
-    private int tamanho=0;
+    private int tamanho;
     
     public v_hashTable(){
         this.vetor = new Veiculo[100];
+        this.tamanho = 0;
     }
 
-    private int hash(int chave){
-        return chave%this.vetor.length;
+    private int hash(int chave, int sond, Veiculo[] vector){
+        return (chave%vector.length + sond)%vector.length; 
     }
 
     private Veiculo[] resize(){
         Veiculo novo[] = new Veiculo[2*this.vetor.length];
         for(int i=0; i<this.vetor.length;i++){
-           put(this.vetor[i].getChassi(), this.vetor[i], novo); 
+            if(this.vetor[i]!=null)
+                rehash(this.vetor[i].getChassi(), this.vetor[i], novo); 
         }
         return novo;
     }
     
-    public void put(int chave, Veiculo valor, Veiculo[] vetor){
+    private void rehash(int chave, Veiculo valor, Veiculo[] vector){
         int sondagem = 0;
         int hash;
-        if(tamanho/vetor.length >= 0.75){
-            Veiculo[] novo;
-            vetor = resize();
+
+        while(sondagem < vector.length){
+            hash = hash(chave,sondagem, vector);
+
+            if(vector[hash] == null){
+                vector[hash] = valor;
+                return;
+            }
+            sondagem++;
         }
-        tamanho++;
+    }
 
-        while(sondagem < vetor.length){
-            hash = (hash(chave) + sondagem)%vetor.length;
+    public void put(int chave, Veiculo valor){
+        int sondagem = 0;
+        int hash;
 
-            if(vetor[hash] == null || vetor[hash].getChassi() == chave){
-                vetor[hash] = valor;
+        if((double)tamanho/this.vetor.length >= 0.75){
+            this.vetor = resize();
+        }
+
+        while(sondagem < this.vetor.length){
+            hash = hash(chave,sondagem, this.vetor);
+
+            if(this.vetor[hash] == null || this.vetor[hash].getChassi() == chave){
+                this.vetor[hash] = valor;
+                tamanho++;
                 return;
             }
             sondagem++;
@@ -45,10 +62,11 @@ public class v_hashTable {
         Veiculo veiculo = this.vetor[hash];
 
         while(sondagem < vetor.length){
-            hash = (hash(chave) + sondagem)%vetor.length;
+            hash = hash(chave,sondagem, this.vetor);
 
             if(vetor[hash].getChassi() == chave){
                 this.vetor[hash] = null;
+                tamanho--;
                 break;
             }
             sondagem++;
@@ -57,21 +75,38 @@ public class v_hashTable {
     }
 
     public Veiculo get(int chave){
-        return this.vetor[hash(chave)];
+        int sondagem = 0;
+        int hash = 0;
+
+        while(sondagem < this.vetor.length){
+            hash = hash(chave,sondagem, this.vetor);
+
+            if(this.vetor[hash].getChassi() == chave){
+                return this.vetor[hash];
+            }
+            sondagem++;
+        }
+        return null;
     }
 
     public int getSize(){
         return this.vetor.length;
     }
 
+    public int getTamanho(){
+        return tamanho;
+    }
+
     public boolean contains(int chave){
         int sondagem = 0;
         int hash = 0;
-        while(sondagem < vetor.length){
-            hash = (hash(chave) + sondagem)%vetor.length;
+        while(sondagem < this.vetor.length){
+            hash = hash(chave,sondagem, this.vetor);
 
-            if(vetor[hash].getChassi() == chave){
-                return true;
+            if(this.vetor[hash] != null){   
+                if(vetor[hash].getChassi() == chave){
+                    return true;
+                }
             }
             sondagem++;
         }
@@ -79,9 +114,5 @@ public class v_hashTable {
     }
 
     
-        
-    
-    
-    
-    
+
 }
